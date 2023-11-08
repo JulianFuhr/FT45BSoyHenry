0d9196d4290075392418d395ed14d428fd50daac
JulianFuhr <julifuhr32@gmail.com>
2023-11-08 10:06:37 -0300

    add: Homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,32 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   if (typeof num !== 'string' || !/^[01]+$/.test(num)) {
+      Error("El valor no es binario");
+   }
+   return parseInt(num, 2);
+}
+console.log(BinarioADecimal('1100'));
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   if(typeof num !== 'number' || isNaN(num ||num<0)){
+      Error("El valor de ser decimal no negativo");
+   
+   }
+   return num.toString(2);
+}
+
+console.log(DecimalABinario(8));
+
+
 
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
 };
+
+
+
+
+
+
